ffd4fae0289591e3e571a5e974dd42f0f4a74b69
thestunter925-stack <thestunter925@gmail.com>
2026-07-18 16:45:17 +0530

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,31 @@
+import { auth } from "./firebase.js";
+
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+
+onAuthStateChanged(auth, (user) => {
+
+  if (!user) {
+    window.location.href = "login.html";
+  }
+
+});
+
+
+window.logout = async function () {
+
+  try {
+
+    await signOut(auth);
+    window.location.href = "login.html";
+
+  } catch (error) {
+
+    alert(error.message);
+
+  }
+
+};
