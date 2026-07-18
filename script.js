7fa42d7f65bc2cf261445fee0eaa3ea499f69806
thestunter925-stack <thestunter925@gmail.com>
2026-07-18 11:21:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 import { db } from "./firebase.js";
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
 // StockPro Inventory Management System
 // JavaScript Functionality
 
