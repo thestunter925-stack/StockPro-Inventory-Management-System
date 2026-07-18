1ce56fb06e2d9e6d35be2095a7a9be54ae4ca4ac
thestunter925-stack <thestunter925@gmail.com>
2026-07-18 15:27:49 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "your_real_key",
-  authDomain: "your_project.firebaseapp.com",
-  projectId: "your_project_id",
-  storageBucket: "your_project.appspot.com",
-  messagingSenderId: "your_sender_id",
-  appId: "your_app_id"
+  apiKey: "AIzaSyChYuucHpUlt43ZDbFiddMGS0AQFLyHP6c",
+  authDomain: "stockpro-inventory-system.firebaseapp.com",
+  projectId: "stockpro-inventory-system",
+  storageBucket: "stockpro-inventory-system.firebasestorage.app",
+  messagingSenderId: "42946254030",
+  appId: "1:42946254030:web:556dcb1d07b711507d2c9f"
 };
 
 const app = initializeApp(firebaseConfig);
